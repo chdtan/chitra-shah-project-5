0dfd72dd88b156a545b07728ed09af4cc987e79c
ChitraS <chdshah@gmail.com>
2019-05-30 19:36:32 -0400

    basic functionality done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 import {Edamam} from './Edamam.js';
+import { thisTypeAnnotation } from '@babel/types';
 
 class APP extends Component {
   constructor() {
     super();
     // Create an empty initial state;
     this.state = {
-      Recipes: '',
+      Recipes: [],
       userInput: '',
     }
   }
@@ -24,35 +26,42 @@ class APP extends Component {
   handleClick = (event) => {
     event.preventDefault();
 
-    // the following setState will empty out the input field after each search
-    // this.setState({
-    //   userInput: ""
-    // })
+    const api_id = "5ad9a9d2";
+    const api_key = "399ab75c6d5b4547cd58b52f7a7d73fd";
+    const { userInput } = this.state;
+
+    axios({
+      method: "GET",
+      url: `https://api.edamam.com/search`,
+      dataResponse: "json",
+      params: {
+        app_id: api_id,
+        app_key: api_key,
+        format: "json",
+        q: userInput,
+        health: "vegan",
+        healthLabels: "vegetarian",
+        recipe: "recipe",
+        
+      }
+    }).then(response => {
+      response = response.data.hits
+      console.log(response);
+
+      // the following setState will empty out the input field after each search - set this after your values are being caught
+      this.setState({
+        Recipes: response
+      })
+
+    })
 
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
   render(){
     return (
       <div className="App"
-      tabindex = "-1">
+      tabIndex = "-1">
         <h1>Vegan Recipes Generator</h1>
         <form action="">
           <input 
@@ -61,11 +70,52 @@ class APP extends Component {
           value = {this.state.value}
           placeholder = "Search by name or ingredient"
           onChange = {this.handleChange}
-          onClick = {this.handleClick}/>
-          <button>Search</button>
+          />
+          <button onClick = {this.handleClick}>Search</button>
         </form>
+      
+
+        {/* <ul>
+          <li>
+            <h2>{this.state.userInput.recipe.ingredients.label}</h2>
+          </li>
+        </ul> */}
+
+      
+          {this.state.Recipes.map(recipe => {
+            console.log(recipe.recipe)
+
+
+            return (
+              <li>
+                <h2>{recipe.recipe.label}</h2>
+                <img src={recipe.recipe.image} alt={recipe.recipe.label}/>
+                  <ul>
+                    {recipe.recipe.ingredients.map((ing, i) => {
+                      return <li>{ing.text}</li>              
+                    })}
+                  </ul>
+                <h2>{recipe.recipe.url}</h2>      
+
+                  {/* <ul>  
+                    {recipe.recipe.ingredientLines.map((steps) => {
+                      return <li></li> 
+                    })}
+                  </ul>  */}
+
+                <ul>
+                  {recipe.recipe.healthLabels.map((label, i)=> {
+                    return <li>{label}</li>
+                  })}
+                </ul>                  
+                <h3> Serving Size: {recipe.recipe.yield}</h3>
+              </li>
+            )          
+          })}       
         
       </div>
+
+
     )
   }
 }
--- src/Edamam.js
+++ src/Edamam.js
@@ -1,24 +1,32 @@
-const api_id = "5ad9a9d2";
-const api_key = "399ab75c6d5b4547cd58b52f7a7d73fd";
-const url = 'https://api.edamam.com/search?'
+// const api_id = "5ad9a9d2";
+// const api_key = "399ab75c6d5b4547cd58b52f7a7d73fd";
+// const url = 'https://api.edamam.com/search?'
 
-const Edamam =
+// class Edamam extends Component {
+//     constructor() {
+//         super();
 
-
-
-    axios ({
-        method: 'GET',
-        url: url,
-        dataResponse: 'json',
-        params: {
-            q: (this.state.UserInput),
-            api_id: api_id,
-            api_key: api_key,
-            Health: 'Vegan',             
-        }
-    }).then(response => {
-        response = 
-    })
+//     }
+   
+// }
 
 
-export default Edamam
+
+//     axios ({
+//         method: 'GET',
+//         url: url,
+//         dataResponse: 'json',
+//         params: {
+//             q: (this.state.UserInput),
+//             api_id: api_id,
+//             api_key: api_key,
+//             Health: 'Vegan',   
+//             count: 5,
+//             recipe: recipe,          
+//         }
+//     }).then(response => {
+//         console.log(response);
+//     })
+
+
+// export default Edamam
