f4b7e29e3579a5e229663163b8c21ba8c5062eb6
ChitraS <chdshah@gmail.com>
2019-06-03 12:36:51 -0400

    Updates

---
+++ precache-manifest.08602cbe8c9b7780e0577330ad59f48f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4821fa3a876ca656b87fd658d584990",
+    "url": "/chitra-shah-project-5/index.html"
+  },
+  {
+    "revision": "e2ed761e9827f4f2ef3d",
+    "url": "/chitra-shah-project-5/static/css/main.63a09230.chunk.css"
+  },
+  {
+    "revision": "87e913273ec8e6e3b27a",
+    "url": "/chitra-shah-project-5/static/js/2.fa9dac84.chunk.js"
+  },
+  {
+    "revision": "e2ed761e9827f4f2ef3d",
+    "url": "/chitra-shah-project-5/static/js/main.54133679.chunk.js"
+  },
+  {
+    "revision": "9fb4fbe073815efc2e53",
+    "url": "/chitra-shah-project-5/static/js/runtime~main.3a68fec1.js"
+  },
+  {
+    "revision": "5b43e6b31ef2bf67c9b72dafd033901f",
+    "url": "/chitra-shah-project-5/static/media/markus-spiske-368800-unsplash.5b43e6b3.jpg"
+  }
+]);
